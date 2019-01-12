1c9b23cc2ca91a85eb4603bb15bdc108ae60dd21
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-12 01:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function iterativeLog(array){
-  array.forEach((index, array) => {
+  array.forEach((index, element) => {
     console.log(`${index}: ${element}`)
   })
+}
+
+function iterate(callback) {
+  const cats = ["Garfield", "Bianca", "Petunia"]
+
+  cats.forEach(callback)
+
+  return cats
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
