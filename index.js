1de06cb78d8e8c00b1482a547615a23293329f83
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-12 01:55:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function iterativeLog(array){
-  array.forEach((index, array) => {
+  array.forEach((index, element) => {
     console.log(`${index}: ${element}`)
   })
+}
+
+function iterate(callback) {
+  const animals = ["dog", "cat", "bird"]
+
+  animals.forEach(callback)
+
+  return animals
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
